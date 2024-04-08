a496084d7cba5e9179d65c5fda71fc22f5f63be1
sullis <github@seansullivan.com>
2024-04-07 18:09:37 -0700

    add eclipse temurin

--- bin/pull-images.sh
+++ bin/pull-images.sh
@@ -4,3 +4,5 @@ docker pull apachepinot/pinot:1.1.0
 docker pull flink:1.19.0-java17
 docker pull confluentinc/cp-kafka:7.2.0
 docker pull apache/kafka:3.7.0
+docker pull eclipse-temurin:17-jdk
+docker pull eclipse-temurin:21-jdk
