c2b4881b33c58e4f58e56a9aedf60659fcfa466a
sullis <github@seansullivan.com>
2024-06-16 15:44:29 -0700

    aws-sdk-rewrite: skip tests

--- bin/aws-sdk-rewrite.sh
+++ bin/aws-sdk-rewrite.sh
@@ -1,3 +1,3 @@
-mvn org.openrewrite.maven:rewrite-maven-plugin:run \
+mvn -DskipTests -Dmaven.javadoc.skip=true org.openrewrite.maven:rewrite-maven-plugin:run \
   -Drewrite.recipeArtifactCoordinates=software.amazon.awssdk:migration-tool:2.23.16-SNAPSHOT
   -Drewrite.activeRecipes=software.amazon.awssdk.UpgradeJavaSdk2
