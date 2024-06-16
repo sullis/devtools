fb1cdccc5ea01f27c9816efcafc4613d501e0577
sullis <github@seansullivan.com>
2024-06-16 15:31:06 -0700

    add script: aws-sdk-rewrite.sh

---
+++ bin/aws-sdk-rewrite.sh
@@ -0,0 +1,3 @@
+mvn org.openrewrite.maven:rewrite-maven-plugin:run \
+  -Drewrite.recipeArtifactCoordinates=software.amazon.awssdk:migration-tool:2.23.16-SNAPSHOT
+  -Drewrite.activeRecipes=software.amazon.awssdk.UpgradeJavaSdk2
