8d7b2761025019fe7a762cc5f31a3623fe014a4a
sullis <github@seansullivan.com>
2024-06-16 15:55:16 -0700

    skip assembly true

--- bin/aws-sdk-rewrite.sh
+++ bin/aws-sdk-rewrite.sh
@@ -1,3 +1,3 @@
-mvn -DskipTests -Dmaven.javadoc.skip=true org.openrewrite.maven:rewrite-maven-plugin:run \
+mvn -Dassembly.skipAssembly=true -DskipTests -Dmaven.javadoc.skip=true org.openrewrite.maven:rewrite-maven-plugin:run \
   -Drewrite.recipeArtifactCoordinates=software.amazon.awssdk:migration-tool:2.23.16-SNAPSHOT
   -Drewrite.activeRecipes=software.amazon.awssdk.UpgradeJavaSdk2
