3d07c593031a965eed9c5e22ae7de78e97f98658
sullis <github@seansullivan.com>
2024-07-27 10:31:10 -0700

    flink 1.19.1

--- bin/pull-images.sh
+++ bin/pull-images.sh
@@ -2,7 +2,7 @@ docker pull mockserver/mockserver
 docker pull localstack/localstack:3.4.0
 docker pull localstack/localstack:s3-latest
 docker pull apachepinot/pinot:1.1.0
-docker pull flink:1.19.0-java17
+docker pull flink:1.19.1-java17
 docker pull confluentinc/cp-kafka:7.2.0
 docker pull apache/kafka:3.7.0
 docker pull eclipse-temurin:17-jdk
