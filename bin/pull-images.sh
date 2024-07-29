5477e3ce87e97d33bfaf8a61f143f9ec420adf56
sullis <github@seansullivan.com>
2024-07-29 12:24:37 -0700

    pull images:  Confluent + Kafka

--- bin/pull-images.sh
+++ bin/pull-images.sh
@@ -4,7 +4,10 @@ docker pull localstack/localstack:s3-latest
 docker pull apachepinot/pinot:1.1.0
 docker pull flink:1.19.1-java17
 docker pull confluentinc/cp-kafka:7.2.0
+docker pull confluentinc/cp-kafka:latest
+docker pull confluentinc/cp-server:latest
 docker pull apache/kafka:3.7.0
+docker pull apache/kafka:3.8.0
 docker pull eclipse-temurin:17-jdk
 docker pull eclipse-temurin:21-jdk
 docker pull mongo
