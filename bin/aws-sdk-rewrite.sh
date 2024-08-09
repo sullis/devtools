4b43304e8b47f4afc6cded7fb9cf379d6b0be86b
sullis <github@seansullivan.com>
2024-08-09 15:13:03 -0700

    aws sdk version

--- bin/aws-sdk-rewrite.sh
+++ bin/aws-sdk-rewrite.sh
@@ -1,3 +1,3 @@
 MAVEN_OPTS="-Xms8g -Xmx8g" mvn -Dassembly.skipAssembly=true -DskipTests -Djacoco.skip -Dcheckstyle.skip -Dmaven.javadoc.skip=true clean install org.openrewrite.maven:rewrite-maven-plugin:run \
-  -Drewrite.recipeArtifactCoordinates=software.amazon.awssdk:migration-tool:2.23.16-SNAPSHOT \
+  -Drewrite.recipeArtifactCoordinates=software.amazon.awssdk:migration-tool:2.27.1 \
   -Drewrite.activeRecipes=software.amazon.awssdk.UpgradeJavaSdk2
