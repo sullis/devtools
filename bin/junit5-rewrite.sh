c3bbc9525af9e576c98aa342ba6edb49a33ffc82
sullis <github@seansullivan.com>
2024-08-30 17:05:49 -0700

    rewrite testing 2.17.0

--- bin/junit5-rewrite.sh
+++ bin/junit5-rewrite.sh
@@ -1,3 +1,3 @@
 MAVEN_OPTS="-Xms8g -Xmx8g" mvn -Dassembly.skipAssembly=true -DskipTests -Djacoco.skip -Dcheckstyle.skip -Dmaven.javadoc.skip=true clean install org.openrewrite.maven:rewrite-maven-plugin:run \
-  -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-testing-frameworks:2.16.0 \
+  -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-testing-frameworks:2.17.0 \
   -Drewrite.activeRecipes=org.openrewrite.java.testing.junit5.JUnit5BestPractices
