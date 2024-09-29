92a3ed3695d19f0452b6d23a2603da6a1422eff7
sullis <github@seansullivan.com>
2024-09-28 21:28:26 -0700

    rewrite

--- bin/jspecify.sh
+++ bin/jspecify.sh
@@ -1,3 +1,3 @@
 MAVEN_OPTS="-Xms8g -Xmx8g" mvn -Dassembly.skipAssembly=true -DskipTests -Djacoco.skip -Dcheckstyle.skip -Dmaven.javadoc.skip=true clean install org.openrewrite.maven:rewrite-maven-plugin:run \
-  -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-migrate-java:2.25.1 \
+  -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-migrate-java:2.26.1 \
   -Drewrite.activeRecipes=org.openrewrite.java.jspecify.MigrateToJspecify
