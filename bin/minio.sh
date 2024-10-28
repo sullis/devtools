5a4f9df3a1678a5854b38c39128f8f10e728ef7d
sullis <seans@grubhub.com>
2024-10-27 19:26:44 -0700

    minio.sh

--- bin/minio.sh
+++ bin/minio.sh
@@ -1,8 +1,3 @@
 mkdir -p ~/minio/data
 
-docker run \
-   -p 9110:9000 \
-   -p 9111:9001 \
-   --name minio \
-   -v ~/minio/data:/data \
-   quay.io/minio/minio server /data --console-address ":9111"
+minio server --address ":9990" --console-address ":9991" ~/minio/data
